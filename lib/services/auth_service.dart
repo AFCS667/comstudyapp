1e2ccedc2d3b63ec06a334675979046b8d97dcc6
YohanesCP <yohaneschristianp176@gmail.com>
2026-05-03 18:22:06 +0700

    fix bug where POST are /users not /auth

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -3,12 +3,12 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 class AuthService {
-  final String baseUrl = dotenv.env['BASE_URL'] ?? 'http://localhost:3000/users';
+  final String baseUrl = dotenv.env['BASE_URL'] ?? 'http://localhost:3000/auth';
 
   // Register a new user
   Future<bool> register(String name, String email, String password) async {
     final response = await http.post(
-      Uri.parse('$baseUrl/register'),
+      Uri.parse('$baseUrl/auth/register'),
       headers: {'Content-Type': 'application/json'},
       body: jsonEncode({'username': name, 'email': email, 'password': password}),
     ).timeout(const Duration(seconds: 10), 
@@ -22,7 +22,7 @@ class AuthService {
   Future<Map<String, dynamic>> login(String email, String password) async {
     try{
       final response = await http.post(
-        Uri.parse('$baseUrl/login'),
+        Uri.parse('$baseUrl/auth/login'),
         headers: {'Content-Type': 'application/json'},
         body: jsonEncode({'email': email, 'password': password}),
       );
