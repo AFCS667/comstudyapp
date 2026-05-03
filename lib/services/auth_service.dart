54d302c0b9260d940143c34c834e145018128d00
YohanesCP <yohaneschristianp176@gmail.com>
2026-05-03 14:59:41 +0700

    add register function

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -2,20 +2,23 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class AuthService {
-  final String baseUrl = "http://10.0.2.2:3000/api";
+  final String baseUrl = "http://10.218.165.43:3000/users";
 
   // Register a new user
-  Future<bool> Register(String name, String email, String password) async {
+  Future<bool> register(String name, String email, String password) async {
     final response = await http.post(
       Uri.parse('$baseUrl/Register'),
       headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'name': name, 'email': email, 'password': password}),
-    );
-    return response.statusCode ==
-        201; // Placeholder for successful registration
+      body: jsonEncode({'username': name, 'email': email, 'password': password}),
+    ).timeout(const Duration(seconds: 10), 
+    onTimeout: () {
+      // Handle timeout
+      return http.Response('Error: Request timed out', 408);
+    });
+    return response.statusCode == 201; // Placeholder for successful registration
   }
 
-  Future<bool?> Login(String email, String password) async {
+  Future<bool?> login(String email, String password) async {
     final response = await http.post(
       Uri.parse('$baseUrl/Login'),
       headers: {'Content-Type': 'application/json'},
